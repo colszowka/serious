af9c8f876e2c4783a2fb49b4a53c73f0b36c67b6
Christoph Olszowka <christoph@olszowka.de>
2010-02-12 12:47:55 +0100

    Tests for archives and index

--- test/test_serious.rb
+++ test/test_serious.rb
@@ -1,11 +1,7 @@
 require 'helper'
 
 class TestSerious < Test::Unit::TestCase
-  def test_my_default
-    get '/'
-    assert_match /ul id="articles"/, last_response.body
-  end
-  
+
   context "GET /" do
     setup { get '/' }
     
@@ -17,4 +13,32 @@ class TestSerious < Test::Unit::TestCase
     should_contain_elements 1, "ul#archives li"
     should_contain_text "Disco 2000", "ul#archives li:first"
   end
+  
+  context "GET /2009" do
+    setup { get '/2009' }
+    
+    should_contain_text "Archives for 2009", "#container h2:first"
+    should_contain_elements 3, "ul#archives li"
+    should_contain_text "Merry Christmas!", "ul#archives li:first"
+    should_contain_text "Ruby is the shit!", "ul#archives"
+    should_contain_text "Foo Bar", "ul#archives"
+  end
+  
+  context "GET /2009/12" do
+    setup { get '/2009/12' }
+    
+    should_contain_text "Archives for 2009-12", "#container h2:first"
+    should_contain_elements 2, "ul#archives li"
+    should_contain_text "Merry Christmas!", "ul#archives li:first"
+    should_contain_text "Ruby is the shit!", "ul#archives"
+  end  
+  
+  context "GET /2009/12/11" do
+    setup { get '/2009/12/11' }
+    
+    should_contain_text "Archives for 2009-12-11", "#container h2:first"
+    should_contain_elements 1, "ul#archives li"
+    should_contain_text "Ruby is the shit!", "ul#archives li:first"
+  end  
+  
 end
