5b32f2a35d9d12e7c960cfe1210cf51f90ef6f21
Christoph Olszowka <christoph@olszowka.de>
2011-08-06 13:13:24 +0200

    Bump version

--- lib/serious/version.rb
+++ lib/serious/version.rb
@@ -2,4 +2,4 @@
 # Serious::VERSION since Serious is a class that inherits from Sinatra::Base
 # and we'd be getting Superclass mismatch errors here since Sinatra is
 # unavailable when evaluating this file standalone...
-SERIOUS_VERSION = '0.3.1'
+SERIOUS_VERSION = '0.3.2'
